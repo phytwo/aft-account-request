61e0824ac95af487454cac010266ba1a35e0b013
Sam Holden <sholden@worth.systems>
2023-06-01 19:31:25 +0100

    dns management accoutn request

---
+++ terraform/002-dns-management-req.tf
@@ -0,0 +1,25 @@
+module "account_request_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cyberwizard42+dnsman@gmail.com"
+    AccountName               = "DNS Management"
+    ManagedOrganizationalUnit = "Infrastructure" 
+    SSOUserEmail              = "cyberwizard42@gmail.com"
+    SSOUserFirstName          = "Cyber"
+    SSOUserLastName           = "Wizard"
+  }
+
+  account_tags = {
+    "001:Owner"       = "cyberwizard42@gmail.com"
+    "001:Division"    = "Management"
+    "001:Environment" = "Production"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Cyber Wizard Operations Management Team."
+    change_reason       = "Creating account for centralised DNS management."
+  }
+
+  account_customizations_name = "PRODUCTION"
+}
